7a54707d31fbde61ddd5f67ddaf86fbea54945ff
Ann Heijkenskjöld <144233838+RockAnnRoll@users.noreply.github.com>
2025-02-20 09:31:31 +0100

    First update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sigmar: ['Urbanist', 'sans-serif'], 
+        },      
+      },
+    },
+    plugins: [],
+   
+  };
+  
